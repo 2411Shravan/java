3265bd145fd5927b3b6df3226c2c7c97f5762903
Shravan .V <perfectparameter24@gmail.com>
2021-08-20 10:57:25 +0530

    interfaces in java

---
+++ inter.java
@@ -0,0 +1,26 @@
+package ex;
+
+
+
+interface abc{
+	void show();
+}
+
+
+
+public class inter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+abc obj=new abc() {
+	public void show() {
+		System.out.println("hai hello");
+	}
+};
+obj.show();
+		
+	}
+
+}
+  
