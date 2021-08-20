e20c10ba186423d15472412cfe3fede31b2d607a
Shravan .V <perfectparameter24@gmail.com>
2021-08-20 11:00:50 +0530

    programs that i coded during my initial java oops learning days

---
+++ abs.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class abs {
+ public int addN(int a, int b) {
+	 return (a+b);
+ }
+ public void displayM() {
+	 System.out.println("Hai hello , i am shravan");
+ }
+}
---
+++ refrence.java
@@ -0,0 +1,13 @@
+import demo.*;
+public class refrence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("User defined packages!!");
+abs obj= new abs();
+int e = obj.addN(6,6);
+System.out.println(e);
+obj.displayM();
+	}
+
+}
